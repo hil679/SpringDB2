1e034ff1ec0ce39376a91926e90ce7cd0505fc52
hil679 <hil679922@gmail.com>
2024-04-21 23:47:46 +0900

    JdbcTemplate 적용1 - 기본

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -0,0 +1,85 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateItemRepositoryV1(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    @Override
+    public Item save(Item item) {
+        String sql = "insert into item (item_name, price, quantity) values (?,?,?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement pstmt = connection.prepareStatement(sql, new String[]{"id"});
+            pstmt.setString(1, item.getItemName());
+            pstmt.setInt(2, item.getPrice());
+            pstmt.setInt(3, item.getQuantity());
+            return pstmt;
+        }, keyHolder);
+
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item set item_name = ?, price = ?, quantity = ? where id = ?";
+        jdbcTemplate.update(sql,
+                updateParam.getItemName(),
+                updateParam.getPrice(),
+                updateParam.getQuantity(),
+                itemId);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select * from item where id = ?";
+        try {
+            Item item = jdbcTemplate.queryForObject(sql, itemRowMapper(), id);
+            return Optional.of(item); // of는 null이면 안 됨
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return ((rs, rowNum) -> {
+            Item item = new Item();
+            item.setId(rs.getLong("id"));
+            item.setItemName(rs.getString("item_name"));
+            item.setPrice(rs.getInt("price"));
+            item.setQuantity(rs.getInt("quantity"));
+
+            return item;
+        });
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        return null;
+    }
+}
