6875cca43fbd369fb7d2a790f4717ff55314d2e9
hil679 <hil679922@gmail.com>
2024-05-12 15:01:48 +0900

    스프링 데이터 JPA 적용1

--- itemservice-db/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -16,5 +16,5 @@ public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
 
     //쿼리 직접 실행
     @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
-    List<Item> findItems(@Param("itemName") String itemName, Integer price);
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
 }
